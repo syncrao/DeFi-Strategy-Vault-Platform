e7900ecb8731435af53ba171a2786d56f188bad5
SyncRao <shahrukhrao1@gmail.com>
2026-01-14 14:58:56 +0530

    add webapp

---
+++ web/src/App.jsx
@@ -0,0 +1,12 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-400">
+        Comming Soon... 🚀
+      </h1>
+    </div>
+  )
+}
+
+

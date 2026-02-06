77b08f6ec2b628600f971c546af99848b54f9a6d
SyncRao <shahrukhrao1@gmail.com>
2026-02-06 16:02:24 +0530

    added wallet connect

--- web/src/layout/Header.jsx
+++ web/src/layout/Header.jsx
@@ -1,17 +1,13 @@
 import { Bell, Menu, Search, Wallet } from "lucide-react";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
-import { useAccount, useChainId, useDisconnect } from "wagmi";
+import { useAccount, useChainId } from "wagmi";
 import { getChainById } from "../utils/chainConfig";
-import { useState } from "react";
 
 export default function Header({ setSidebarOpen }) {
   const { open } = useWeb3Modal();
-  const chainId = useChainId();
+  const chainId = useChainId()
   const { address, isConnected } = useAccount();
-  const chain = getChainById(chainId);
-
-  const { disconnect } = useDisconnect();
-  const [connecting, setConnecting] = useState(false);
+  const chain = getChainById(chainId)
 
   return (
     <div className="bg-white/10 px-6 py-2 border-b border-white/20  backdrop-blur-xl">
@@ -37,48 +33,19 @@ export default function Header({ setSidebarOpen }) {
             <button className="relative p-3 text-white/70 hover:text-white hover:bg-white/10 rounded-2xl transition-all duration-300 group">
               <Bell className="h-6 w-6" />
               <span className="absolute -top-0 -right-0 h-5 w-5 bg-gradient-to-r from-pink-500 to-red-500 rounded-full text-xs text-white flex items-center justify-center">
-                3
+                3 
               </span>
             </button>
-            <button
-              onClick={async () => {
-                if (connecting) return;
-
-                setConnecting(true);
-
-                try {
-                  await disconnect();
-
-                  localStorage.removeItem("walletconnect");
-                  localStorage.removeItem("wagmi.store");
-                  localStorage.removeItem("wagmi.cache");
-
-                
-                  setTimeout(() => {
-                    open();
-                    setConnecting(false);
-                  }, 300);
-                } catch (err) {
-                  open();
-                  setConnecting(false);
-                }
-              }}
-              className="flex items-center space-x-3  bg-white/10 backdrop-blur-xl rounded-2xl px-4 py-2 border border-white/20 hover:bg-white/20 transition-all duration-300 cursur-pointer group"
-            >
+            <button onClick={() => open()} className="flex items-center space-x-3  bg-white/10 backdrop-blur-xl rounded-2xl px-4 py-2 border border-white/20 hover:bg-white/20 transition-all duration-300 cursur-pointer group">
               <div className="text-right ">
-                <p className="text-white font-semibold">
-                  {isConnected
-                    ? `${address.slice(0, 4)}...${address.slice(-4)}`
-                    : "Connect"}
-                </p>
-                <p className="text-white/60 text-xs">
-                  {isConnected
-                    ? `${chain?.name ?? "Unknown Network"}`
-                    : "Wallet"}
-                </p>
+                <p className="text-white font-semibold">{ isConnected ? `${address.slice(0, 4)}...${address.slice(-4)}` : "Connect"}</p>
+                <p className="text-white/60 text-xs">{isConnected ? `${chain?.name ?? "Unknown Network"}` : "Wallet"}</p>
               </div>
               <div className="relative">
-                <Wallet className="text-white font-bold w-8 h-8" />
+                
+                
+                  <Wallet  className="text-white font-bold w-8 h-8"/>
+                
               </div>
             </button>
           </div>
